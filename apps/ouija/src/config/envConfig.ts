45f5db40251009025bcc839fe0aae67be4d46577
fga97 <fgarrec0397@gmail.com>
2023-10-26 20:15:48 -0400

    fix: build

--- apps/ouija/src/config/envConfig.ts
+++ apps/ouija/src/config/envConfig.ts
@@ -3,9 +3,5 @@ const getEnv = () => process.env;
 export const getOpenAiApiKey = () => {
     const apiKey = getEnv().OPENAI_API_KEY;
 
-    if (!apiKey) {
-        throw Error("OPENAI_API_KEY is not set");
-    }
-
     return apiKey;
 };

01091d5ac8ea21b5188721cdb017e1ee035c302d
fga97 <fgarrec0397@gmail.com>
2023-11-07 20:23:11 -0500

    ci: removed old gh action

--- apps/audio-encoder/src/controllers/okController.ts
+++ apps/audio-encoder/src/controllers/okController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 const get = async (request: Request, result: Response) => {
-    result.status(200).json({ result: "The app is up and running" });
+    result.status(200).json({ result: "The app is up and running. Version 2" });
 };
 
 export default {

ddb3eb43a64e60c2c92793ee344d4da0a145e0e3
fga97 <fgarrec0397@gmail.com>
2024-02-01 21:33:11 -0500

    refactor(WIP): bringing legacy services into nestjs

--- services/videos-factory-service/src/modules/video/video.controller.ts
+++ services/videos-factory-service/src/modules/video/video.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Post } from "@nestjs/common";
 
+import { Script } from "./components/ScriptManager/ScriptManager";
 import { ScriptService } from "./services/script.service";
 import { TemplateService } from "./services/template.service";
 import { VideoService } from "./services/video.service";
+import { Template } from "./videoTypes";
+
+const canGenerateScript = true;
+const canGenerateTemplate = true;
+const canRenderVideo = true;
 
 @Controller("video")
 export class VideoController {
@@ -14,7 +20,34 @@ export class VideoController {
 
     @Post()
     async createVideo() {
-        console.log("create a video");
+        let script: Script = {};
+        let template: Template | undefined = undefined;
+
+        if (canGenerateScript) {
+            script = await this.scriptService.generateScript();
+        }
+
+        if (canGenerateTemplate) {
+            this.templateService.prepareTemplate(script);
+            template = await this.templateService.createTemplate();
+        }
+
+        try {
+            if (!canRenderVideo) {
+                result.status(200).json({ result: "Video not created" });
+                return;
+            }
+
+            if (template) {
+                await this.videoService.generateVideo(template);
+            }
+
+            result.status(200).json({ result: "Video created" });
+        } catch (error) {
+            console.log(error, "error");
+
+            result.status(500).json({ error });
+        }
 
         return "create a video";
     }
